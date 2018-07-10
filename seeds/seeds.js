00306dee96ca885127295cb460cbf1a6d7f56bfe
Kin Siu <kina.siu@gmail.com>
2018-07-10 10:40:12 -0400

    changed seed file data

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -5,9 +5,8 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('notes').insert([
-        {address: 'kina.siu@gmail.com', carrier: 'AT&T', subject: 'email head', message: 'hello email!', time:'1989-04-13T13:53:00.000Z', timeNum: '608478780000'},
-        {address: '7326983973', carrier: 'AT&T', subject: 'text head', message: 'hello, text', time:'1989-04-13T13:53:00.000Z', timeNum: '1524750840000'},
-        {address: '1234567890', carrier: 'T-Mobile', subject: 'Lonely MILFS in your area!', message: 'why would you click on this?', time:'2018-04-26T22:00:00.000Z', timeNum: '1524780000000'}
+        {address: 'YOUR CELL NUMBER HERE', carrier: '@txt.att.net', subject: 'text head', message: 'hello, text', time:'1989-04-13T13:53:00.000Z', timeNum: '1524750840000'},
+        {address: 'YOUR EMAIL HERE', carrier: '', subject: 'Lonely MILFS in your area!', message: 'why would you click on this?', time:'2018-04-26T22:00:00.000Z', timeNum: '1524780000000'}
       ]);
     });
 };
